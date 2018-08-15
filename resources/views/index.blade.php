217681ece7e4083f8ddf28b56cb852d72f3d45a1
CalvinL42 <30641266+CalvinL42@users.noreply.github.com>
2018-08-15 17:00:58 +1200

    First stage of setting up views
    some css working for index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Fight For Kidz 2018</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+
+  <!-- Custom styles for this template -->  
+  <link href="css/custom.css" rel="stylesheet">
+  <link href="css/grayscale.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="index.html#page-top">
+        <img src="img/f4k.png" class="img-fluid" alt="Fight For Kidz">
+      </a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+        aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="index.html#about">About us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="contenders.html">Contenders</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="previous.html">Previous Years</a>
+          </li>
+          <li class="dropdown">
+            <a class="dropdown-toggle nav-link" data-toggle="dropdown" href="#">Support Us
+              <span class="caret"></span>
+            </a>
+            <ul id="dropdown2" class="dropdown-menu">
+              <li>
+                <a class="js-scroll-trigger" href="#"><p>Reserve Seats</p></a>
+              </li>
+              <li>
+                <a class="js-scroll-trigger" href="#merchandise"><p>Merchandise</p></a>
+              </li>
+              <li>
+                <a href="auction.html"><p>Auction</p></a>
+              </li>
+              <li>
+                <a class="js-scroll-trigger" href="index.html#signup"><p>Apply</p></a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="index.html#signup"><p>Contact Us</p></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <img src="img/f4k.png" class="img-fluid" />
+        <h2 class="text-white-50 mx-auto mt-5 mb-5">Fight For Kidz is a charity boxing event held in Southland to raise money for Southland kidz charities.</h2>
+        <a href="#signup" class="btn btn-primary js-scroll-trigger">Be a part of it!</a>
+      </div>
+    </div>
+  </header>
+
+  <!-- Upcoming event -->
+  <section class="upcoming-section">
+    <div class="container">
+      <h1 class="text-white">Fight for Kidz 2018</h1>
+      <p class="text-white mb-0">Join us on April 28th 2018 for the next Fight For Kids!</p>
+    </div>
+  </section>
+
+  <!-- About Section -->
+  <section id="about" class="about-section">
+    <div class="container">
+      <h1 class="text-white text-center mb-5">About Us</h1>
+      <div class="row mb-5">
+        <div class="col-lg-7">
+          <h4 class="bar text-left text-white">Our Vision</h4>
+          <div class="mt-lg-5">
+            <p class="text-white-50 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae deserunt ab cupiditate quidem qui voluptates dolores
+              quo veniam tempora neque sapiente libero ullam, excepturi culpa quibusdam non tempore! Quis, consequatur. Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Repellendus maxime ducimus nulla veritatis quia aliquam
+              vel architecto amet doloribus laudantium neque ipsum nemo, accusantium cupiditate et. Tempora eaque hic perspiciatis!
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-2"></div>
+        <div class="col-lg-3 text-white text-right results">
+          <h4 class="bar">Our Results - 2017</h4>
+          <p class="all-caps">Contenders</p>
+          <p class="stat">12</p>
+          <p class="all-caps">Funds Raised</p>
+          <p class="stat">$212,000</p>
+          <p class="all-caps">Charities Supported</p>
+          <p class="stat">CharityOne, CharityTwo</p>
+        </div>
+      </div>
+      <img src="img/troopers-memorial2.png" class="img-fluid mb-5" />
+    </div>
+  </section> 
+
+  <section id="sponsors-section">
+    <h2 class="text-center">Our Sponsors</h2>
+      <div class="row">
+        <div class="col-lg-3"><img src="img/customer-1.png" /></div>
+        <div class="col-lg-3"><img src="img/customer-2.png" /></div>
+        <div class="col-lg-3"><img src="img/customer-3.png" /></div>
+        <div class="col-lg-3"><img src="img/customer-4.png" /></div>
+      </div>
+    </section>      
+
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="bg-black small text-center text-white-50">
+    <div class="container">
+      Copyright &copy; Fight For Kidz 2018
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/font-awesome/js/all.js"></script>
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/grayscale.js"></script>
+
+</body>
+
+</html>
