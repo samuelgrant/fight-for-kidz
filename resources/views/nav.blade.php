ac89fa89f22ff0d2e578f89ab269260dc6fb6b91
CalvinL42 <coyo999@outlook.com>
2018-08-16 09:30:15 +1200

    Create nav.blade.php

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,48 @@
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="index.html#page-top">
+        <img src="img/f4k.png" class="img-fluid" alt="Fight For Kidz">
+      </a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+        aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="index.html#about">About us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="/contenders">Contenders</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="/previous">Previous Years</a>
+          </li>
+          <li class="dropdown">
+            <a class="dropdown-toggle nav-link" data-toggle="dropdown" href="#">Support Us
+              <span class="caret"></span>
+            </a>
+            <ul id="dropdown2" class="dropdown-menu">
+              <li>
+                <a class="js-scroll-trigger" href="#"><p>Reserve Seats</p></a>
+              </li>
+              <li>
+                <a class="js-scroll-trigger" href="#merchandise"><p>Merchandise</p></a>
+              </li>
+              <li>
+                <a href="/auction"><p>Auction</p></a>
+              </li>
+              <li>
+                <a class="js-scroll-trigger" href="index.html#signup"><p>Apply</p></a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="index.html#signup"><p>Contact Us</p></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
