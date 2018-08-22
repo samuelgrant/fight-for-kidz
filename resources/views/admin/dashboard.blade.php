06ea851c106b099162d3db96b7c3e1acba770247
MitchellQ <40969453+MitchellQ@users.noreply.github.com>
2018-08-22 13:20:08 +1200

    added new view admin
    - created dashboard.blade.php
    -  created template files for head, nav and footer

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.layouts.app')
+@section('page')
+<!-- Breadcrumbs-->
+<ol class="breadcrumb">
+    <li class="breadcrumb-item">
+        <a href="index.html">Dashboard</a>
+    </li>
+    <li class="breadcrumb-item active">404 Error</li>
+</ol>
+
+<!-- Page Content -->
+
+@endsection
