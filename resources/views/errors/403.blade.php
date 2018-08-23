f046062af1ae29ba9706ffae9e702e90ce5b2fec
CalvinL42 <30641266+CalvinL42@users.noreply.github.com>
2018-08-23 14:28:57 +1200

    added error pages & fixed nav

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container auth">
+    <div class="text-center text-white">
+        <i class="fas fa-user-lock fa-5x"></i>
+        <h1 class="pt-2 mb-1">Forbidden</h1>
+        <span>You are not allowed to access this page. If you believe this is an error, contact a site administrator.</span>
+    </div>    
+</div>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container auth">
+    <div class="text-center text-white">
+        <i class="fas fa-exclamation-triangle fa-5x"></i>
+        <h1 class="pt-2 mb-1">Page Not Found</h1>
+    </div>    
+</div>
+@endsection
