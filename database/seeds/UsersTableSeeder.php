8fb120dac01c396cbff4d1677ab723517e26c634
jacksos101 <jacksos101@gmail.com>
2018-08-27 11:22:35 +1200

    Added users seeder
    This seeder creates two example users that can be used to test the sites functions.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,13 +5,32 @@ use Illuminate\Database\Seeder;
 class UsersTableSeeder extends Seeder
 {
     /**
-     * Creates fake user records as defined in /database/factories/UserFactory.php
+     * This class creates two users for testing purposes.
+     * 
+     * Use the command 'php artisan db:seed' to execute this seeded along with any others. 
+     * Or use 'php artisan db:seed class=UsersTableSeeder' to execute this seeder only.
      *
      * @return void
      */
     public function run()
     {
         
-        factory(App\User::class, 50)->create();
+        factory(App\User::class)->create(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+                'password' => '$2y$10$6z.fpsPw33atdIbj6mwhKevVR.lYRKrjxNxBBtS4XPmun9r4yOkzi',
+                'active' => 1
+            ]
+        );
+
+        factory(App\User::class)->create(
+            [
+                'name' => 'Tester',
+                'email' => 'tester@example.com',
+                'password' => '$2y$10$6z.fpsPw33atdIbj6mwhKevVR.lYRKrjxNxBBtS4XPmun9r4yOkzi',
+                'active' => 0
+            ]
+        );
     }
 }
