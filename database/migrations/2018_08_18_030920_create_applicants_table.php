ff4fe3e0f050f607726d6a611f8ac65c27fbe90d
jacksos101 <jacksos101@gmail.com>
2018-08-29 23:51:04 +1200

    Update applicants migration with new name

--- database/migrations/2018_08_18_030920_create_applicants_table.php
+++ database/migrations/2018_08_18_030920_create_applicants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateContenderApplicationsTable extends Migration
+class CreateApplicantsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateContenderApplicationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('contender_applications', function (Blueprint $table) {
+        Schema::create('applicants', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('event_id')->unsigned();
             $table->boolean('is_selected');            
@@ -35,6 +35,6 @@ class CreateContenderApplicationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contender_applications');
+        Schema::dropIfExists('applicants');
     }
 }
