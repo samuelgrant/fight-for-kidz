57b5f7f4fb55ec64b920c34414a3e341f7e0187b
jacksos101 <jacksos101@gmail.com>
2018-08-30 00:19:53 +1200

    Added contacts() function to group model
    This function collects all group members from the different tables and returns an associative array containing each group members name and email. Each members role (sponsor, applicant, user) is also included for convenience.

--- app/Group.php
+++ app/Group.php
@@ -18,4 +18,30 @@ class Group extends Model
     public function users(){
         return $this->belongsToMany('App\User', 'group_user');
     }
+
+    /**
+     *  Returns an array of associative arrays, each one containing the role, name and email of 
+     *  mail group member.
+     * 
+     * @return array
+     */
+    public function contacts(){
+        $applicants = $this->applicants();
+        $sponsors = $this->sponsors();
+        $users = $this->users();
+
+        foreach($applicants as $applicant){
+            $contacts[] = ['role' => 'applicant', 'name' => $applicant->name, 'email' => $applicant->email];
+        }
+
+        foreach($sponsors as $sponsor){
+            $contacts[] = ['role' => 'sponsor', 'name' => $sponsor->name, 'email' => $sponsor->email];
+        }
+
+        foreach($users as $user){
+            $contacts[] = ['role' => 'user', 'name' => $user->name, 'email' => $user->email];
+        }
+
+        return $contacts;
+    }
 }
