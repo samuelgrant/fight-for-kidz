40a41c3a1630c2f0de011ae4ba882077a7b320b8
jacksos101 <jacksos101@gmail.com>
2018-08-29 23:54:52 +1200

    Added group migrations
    Including main groups table, plus pivot tables for users, contenders and sponsors. Email contacts not belonging to any of these groups are yet to be catered for

---
+++ database/migrations/2018_08_29_110532_create_group_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * 
+     * This table serves as a pivot table between the many to many relationship between groups and users
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_user', function (Blueprint $table) {
+            $table->unsignedInteger('group_id');
+            $table->unsignedInteger('user_id');
+
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $talbe->primary('group_id', 'user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_user');
+    }
+}
