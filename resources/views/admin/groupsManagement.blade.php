3accab99767440243961d7f75641585d85062bcb
jacksos101 <jacksos101@gmail.com>
2018-09-05 23:53:33 +1200

    Improved groups management view
    - Fixed issues with group names overlapping
    - Shifted the 'new group' button to a different row
    - Removed nested row and tweaked col sizes
    
    This commit will likely conflict with Mitchells pull request so we will have to work through that.

--- resources/views/admin/groupsManagement.blade.php
+++ resources/views/admin/groupsManagement.blade.php
@@ -1,29 +1,28 @@
-@extends('admin.layouts.app')
-@section('page')
+@extends('admin.layouts.app') @section('page')
 <!-- Breadcrumbs-->
 <ol class="breadcrumb">
     <li class="breadcrumb-item">
         <a href="{{route('admin.dashboard')}}">Dashboard</a>
     </li>
-    <li class="breadcrumb-item active">Group Management</li>   
+    <li class="breadcrumb-item active">Group Management</li>
 </ol>
 
 <!-- Page Content -->
 <div class="row">
     <div class="col-md-12">
         <button class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#newGroup">New Group</button>
-        <div class="row">
-            @foreach($groups as $group)
-            <div class="col-md-4 col-md-4 col-sm-6 my-1">    
-                <a class="btn groups" href="{{ route('admin.group', ['id' => $group->id])}}">
-                    <img src="/storage/images/groups/{{($group->custom_icon)?$group->id: 0 }}.png" alt="Group Icon"/>
-                    <h3>{{$group->name}}</h3>
-                    <span class="d-block">{{$group->type}}</span>
-                </a>
-            </div>
-            @endforeach
-        </div>
     </div>
+</div>
+<div class="row">
+    @foreach($groups as $group)
+    <div class="col-lg-3 col-md-4 col-sm-6 my-4 px-2">
+        <a class="btn groups" href="{{ route('admin.group', ['id' => $group->id])}}">
+            <div><img class="d-block m-auto" src="/storage/images/groups/{{($group->custom_icon)?$group->id: 0 }}.png" alt="Group Icon" /></div>
+            <h5>{{$group->name}}</h5>
+            <span class="d-block text-center">{{$group->type}}</span>
+        </a>
+    </div>
+    @endforeach
 </div>
 
 <!-- New Group Modal -->
@@ -31,31 +30,35 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header bg-dark text-white">
-                <h4 class="modal-title">Create a New Group</h4><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+                <h4 class="modal-title">Create a New Group</h4><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">×</span></button></div>
             <div class="modal-body">
-                {!! Form::open(['action' => 'admin\GroupManagementController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-                    <div class="form-group">
-                      <label for="name">Group Name</label>
-                      <input type="text" name="groupName" id="name" class="form-control" placeholder="*required" maxlength="30" required>
+                {!! Form::open(['action' => 'admin\GroupManagementController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data'])
+                !!}
+                <div class="form-group">
+                    <label for="name">Group Name</label>
+                    <input type="text" name="groupName" id="name" class="form-control" placeholder="*required" maxlength="30" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="groupAvatar">Optional Group Icon</label>
+                    <div>
+                        <img id="imgPreview" src="https://via.placeholder.com/100x80" class="float-left mr-2" alt="placeholder">
+                        <label class="btn btn-info btn-sm btn-file">
+                            <i class="fas fa-upload"></i> Select Image
+                            <input name="groupImage" id="img" type="file" style="display: none;">
+                        </label>
+                        <button class="btn btn-danger btn-sm d-block" type="button" onclick="resetImagePre()"><i class="fas fa-times"></i>
+                            Remove Image</button>
                     </div>
-                    
-                   <div class="form-group">
-                        <label for="groupAvatar">Optional Group Icon</label>
-                        <div>
-                            <img id="imgPreview" src="https://via.placeholder.com/100x80" class="float-left mr-2" alt="placeholder">
-                            <label class="btn btn-info btn-sm btn-file">
-                                <i class="fas fa-upload"></i> Select Image 
-                                <input name="groupImage" id="img" type="file" style="display: none;">
-                            </label>
-                            <button class="btn btn-danger btn-sm d-block" type="button" onclick="resetImagePre()"><i class="fas fa-times"></i> Remove Image</button>
-                        </div>
-                        <small id="groupAvatarHelp" class="text-muted d-block">Required format .png - max 100 x 80 px.</small>
-                   </div>
-                   <style>
-                   .btn-file {
+                    <small id="groupAvatarHelp" class="text-muted d-block">Required format .png - max 100 x 80 px.</small>
+                </div>
+                <style>
+                    .btn-file {
                         position: relative;
                         overflow: hidden;
                     }
+
                     .btn-file input[type=file] {
                         position: absolute;
                         top: 0;
@@ -71,8 +74,8 @@
                         cursor: inherit;
                         display: block;
                     }
-                   </style>
-                    <button class="btn btn-primary btn-success" type="submit">Create Group</button>
+                </style>
+                <button class="btn btn-primary btn-success" type="submit">Create Group</button>
 
                 {!! Form::close() !!}
             </div>
