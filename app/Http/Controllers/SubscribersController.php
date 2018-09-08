f9f0be6f46e1eb67be5d0a390d524955108795b0
Samuel Grant <samueljegrant@outlook.com>
2018-09-08 22:52:07 +1200

    Hot Fix - Use input.

--- app/Http/Controllers/SubscribersController.php
+++ app/Http/Controllers/SubscribersController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Input;
 use App\Subscriber;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
--- config/app.php
+++ config/app.php
@@ -213,6 +213,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Input' => Illuminate\Support\Facades\Input::class,
     ],
 
 ];
