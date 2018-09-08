0a9a9e79a911782c32aa700ffd5ca7b94e9b6d93
Samuel Grant <samueljegrant@outlook.com>
2018-09-08 22:46:42 +1200

    Fixed a missing import for Validator

--- app/Http/Controllers/SubscribersController.php
+++ app/Http/Controllers/SubscribersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Subscriber;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 
 class SubscribersController extends Controller
 {
