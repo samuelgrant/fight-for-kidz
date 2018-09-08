bff48ce8ddf7d9317312205906222b59ba9a5bd4
jacksos101 <jacksos101@gmail.com>
2018-09-08 18:24:35 +1200

    Contact->role now defaults to 'Custom Contact' , non nullable

--- database/migrations/2018_09_03_234657_create_contacts_table.php
+++ database/migrations/2018_09_03_234657_create_contacts_table.php
@@ -17,7 +17,7 @@ class CreateContactsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email');
-            $table->string('role')->nullable();
+            $table->string('role')->default('Custom Contact');
             $table->timestamps();
         });
     }
