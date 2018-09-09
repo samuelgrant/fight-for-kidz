04ce274c541d291b297ea2509b84fed3fad7ecb0
jacksos101 <jacksos101@gmail.com>
2018-09-09 21:19:52 +1200

    Prevented adding groupable to group > once
    Utilizes the primary key constraint on the table and the resultant SQL exception if a duplicate record is attempted. I was unable to find an elegant way of checking through eloquent alone, due to the polymorphic nature of these relationships.
    
    This checking is not required for detach() as this will naturally have no effect if the record doesn't exist.

--- app/Traits/Groupable.php
+++ app/Traits/Groupable.php
@@ -15,29 +15,56 @@ use App\Group, App\User, App\Subscriber, App\Sponsor, App\Contact, App\Applicant
 
 trait Groupable
 {
+    /**
+     *  Adds the groupable object to a group based off of the groups ID.
+     * 
+     *  Tries adding the groupable to the group, if the database throws an exception
+     *  becuase the record already exists, then catch the exception and return null.
+     * 
+     * @return void
+     */
     public function addToGroup($groupId)
     {
-        
-    // if not already in group, add to group
 
-    $this->groups()->attach($groupId);
+        try {
+            $this->groups()->attach($groupId);
 
-    Log::debug('Added '.get_class($this).' '.$this->id. ' to group '.$groupId);
+            Log::debug('Added ' . get_class($this) . ' ' . $this->id . ' to group ' . $groupId);
 
+        } catch (\PDOException $ex) {
+            
+            // code 23000 is an constraint / integrity violation
+            if($ex->getCode() == 23000){
+                Log::debug(get_class($this).' '.$this->id. ' is already in group '.$groupId);
+                return;
+            }
+            else { // continue to throw the exception if it isn't related to duplicate record.
+                throw $ex;
+            }
+        }
     }
 
+    /**
+     *  Removes the groupable from a group based off of the groups ID. 
+     * 
+     * @return void
+     */
     public function removeFromGroup($groupId)
     {
+        $this->groups()->detach($groupId);
 
-    // if in group, remove from group
-
-    $this->groups()->detach($groupId);
-
-    Log::debug('Removed '.get_class($this).' '.$this->id.' from group '.$groupId);
+        Log::debug('Removed ' . get_class($this) . ' ' . $this->id . ' from group ' . $groupId);
 
     }
 
-    public function groups(){
+    /**
+     * Returns an Eloquent collection of the group objects to which this groupable
+     * is a member.
+     * 
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function groups()
+    {
         return $this->morphToMany('App\Group', 'groupable');
     }
 }
