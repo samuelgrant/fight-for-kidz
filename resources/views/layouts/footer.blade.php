4038c059a396c88e1ae4fe45aef50721a6a72fe0
MitchellQ <40969453+MitchellQ@users.noreply.github.com>
2018-09-10 16:10:51 +1200

    added a cookie popup and adjusted footer
    - attempting to comply with GDPR requirements just in case
    
    - moved cookie popup to the side of the captcha logo
    
    - removed not neeed button from the cookie popup
    
    - added f4k logo to the footer this will also need to be adjusted each year or can be removed entirely

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,20 @@
 <!-- Footer -->
+
+<!-- I have cookies -->
+<script>
+  $('body').ihavecookies({
+    title: "Cookies & Privacy",
+    message: "This website uses cookies to ensure you get the best experience on our website.",
+    cookieTypes: []
+  }); 
+</script>
+
   <footer class="bg-black small text-center text-white-50">
+    <div class="col-sm-4">
+      <img src="img/f4k.png" style="height:50px" alt="Fight for Kidz logo" class="d-none d-md-inline-block">
+    </div>
     <div class="container">
-          Copyright &copy; {{config('app.name')}} {{ date('Y') }}
+      Copyright &copy; {{config('app.name')}} {{ date('Y') }}
     </div>
     <div class="text-right mr-3">
       <a style="color: grey;" href="{{route('login')}}">Administration Login</a>
