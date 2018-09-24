0a735887517e87e035941b7f00f13269c85b8544
Samuel Grant <samueljegrant@outlook.com>
2018-09-25 10:52:55 +1200

    Inital work on nav bar and public page with no event

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,102 +8,40 @@
         <div>
           <img src="/storage/images/f4k_logo.png" class="img-fluid">
         </div>  
-        <h2 class="text-white-50 mx-auto mt-5 mb-5">Fight for Kidz is a charity boxing event held in Southland to raise money for Southland kidz charities.</h2>
+        <div class="text-white-50 mx-auto mt-5 mb-5">
+            <button class="bout-btn bout-btn-red">About Us</button>
+            <button class="bout-btn bout-btn-blue">Fight for Kidz 2018</button>
+        </div>
       </div>
     </div>
   </header>
   <!-- Upcoming event -->
   <div style="background-color: black;">
-    <section class="upcoming-section">
+    <section id="about" class="upcoming-section">
       <div class="container">
         <div class="row mb-5">
           <div class="col-lg-8 col-md-6 col-col-sm-12">
-            <h1 class="text-white underline bar">{{$event->name}}</h1>
-            <p class="text-justify">{!! $event->desc_1 !!}</p>
+            <h1 class="text-white underline bar">About</h1>
+            <p class="text-justify">
+                Fight for Kidz is a charity boxing event held in Southland every year to help raise funds for our most vunerable children.
+                Since 2003 boxers have gone head to head in xx events  raising money for verious charities rasing a total of $xxx,xxx.
+            </p>
+
+            <p class="texr-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae deserunt ab cupiditate quidem qui voluptates dolores quo veniam tempora neque sapiente libero ullam, 
+                excepturi culpa quibusdam non tempore! Quis, consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus maxime ducimus nulla veritatis quia aliquam vel architecto amet doloribus laudantium neque ipsum nemo, accusantium cupiditate et. Tempora eaque hic perspiciatis!
+            </p>
           </div>
-          <div class="col-lg-4 col-md-6 col-sm-12 text-white text-right results mt-5">
-              <p class="all-caps sidebar-heading">Date</p>
-              <p class="stat">{{\Carbon\Carbon::parse($event->datetime)->format('D d M Y')}}</p>
-              <p class="all-caps sidebar-heading">Location</p>
-          <p class="stat">{{$event->venue_name}}</p>
-              <p class="all-caps sidebar-heading">Supporting</p>
-                  @if($event->charity_url)
-                  <a class="stat-link" href="{{$event->charity_url}}" target="blank" style="color: white!important;">
-                    <i class="fas fa-link"></i> {{$event->charity}}
-                  </a>
-                  @else 
-                    <p class="stat">{{$event->charity}}</p>
-                  @endif
+          <div class="col-lg-4 col-md-6 col-sm-12 mt-5 pb-4">
+            <div id="fb-root"></div>
+                <div class="fb-page" data-href="https://www.facebook.com/fightforkidz" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                    <blockquote cite="https://www.facebook.com/fightforkidz" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/fightforkidz">Fight For Kidz</a></blockquote>
+                </div>
             </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-12 col-md-12 col-col-sm-12">
-            <div id="map" style="width:100%; height: 450px; border:0"></div>
-                <script>
-                    function initMap() {
-                        var uluru = { {{$event->venue_gps}} };
-                        var map = new google.maps.Map(document.getElementById('map'), {
-                        zoom: 15,
-                        center: uluru
-                        });
-                        var marker = new google.maps.Marker({
-                        position: uluru,
-                        map: map
-                        });
-                    }
-                </script>
-                
-                <script async defer
-                    src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_MAPS_KEY')}}&callback=initMap">
-                </script>
-            </div> 
-          </div>
-        </div>    
+        </div> 
       </div>
       <hr>
     </section>
-
-  <!-- Subscriber Section -->
-  <section class="text-center" id="subscriber-section">
-
-    <div class="container my-5">
-      <h1 class="mb-3">Fight for Kidz Newsletter!</h1>
-      @include('layouts.messages')
-      <form method="post" action="{{route('subscribe')}}" class="justify-content-center">
-        <div class="row">
-          <div class="col-md-6 col-sm-12">
-            <div class="form-group">
-              <label for="name">Your Name</label>
-              <input type="text" name="name" id="name" class="form-control" required>
-            </div>
-          </div>
-          <div class="col-md-6 col-sm-12">
-            <div class="form-group">
-              <label for="email">Email</label>
-              <input type="email" name="email" id="email" class="form-control" required>
-            </div>
-          </div>
-
-          <div class="col-md-6 offset-md-3">         
-            <button class="btn btn-danger btn-block " id="subscribeBtn" type="submit"><i class="fas fa-user-plus"></i> Sign Up</button>
-            {!! app('captcha')->render(); !!}      
-          </div>
-        </div>
-        @csrf
-      </form>
-    </div>
-  </section>
-
-    <!-- Sponsors Section -->
-    <section id="sponsors-section">
-      <h2 class="text-center text-dark">Our Sponsors</h2>
-      <div class="slick-sponsors">
-          <div><img src="img/customer-1.png" /></div>
-          <div><img src="img/customer-2.png" /></div>
-          <div><img src="img/customer-3.png" /></div>
-          <div><img src="img/customer-4.png" /></div>
-      </div>
-      </section>    
   </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,11 @@ Route::get('/contenders', 'PagesController@contenders')->name('contenders');
 Route::get('/contact', 'PagesController@contact')->name('contact');
 Route::get('/about', 'PagesController@about')->name('about');
 
-Route::get('/Merchandise', 'MerchandiseController@Merchandise')->name('Merchandise');
+Route::get('/Merchandise', 'MerchandiseController@Merchandise')->name('merchandise');
 
-Route::get('/apply', 'EventApplicationController@index')->name('application');
-Route::post('/apply', 'EventApplicationController@store')->name('apply');
+Route::get('/fighter-application', 'EventApplicationController@index')->name('application.fight');
+Route::get('/sponsor-application', 'EventApplicationController@index')->name('application.sponsor');
+Route::post('/apply-to-fight', 'EventApplicationController@store')->name('apply');
 
 // Subscriber route
 Route::post('/subscribe', 'SubscribersController@store')->name('subscribe');
