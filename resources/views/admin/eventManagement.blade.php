db23b328b0720484766a4803dc17855c63fbde8c
jacksos101 <jacksos101@gmail.com>
2018-10-18 13:30:55 +1300

    WIP Event Overview

--- resources/views/admin/eventManagement.blade.php
+++ resources/views/admin/eventManagement.blade.php
@@ -29,22 +29,66 @@
             </ul>
         </div>
         <div class="tab-content">
-            <div class="tab-pane " role="tabpanel" id="tab-1">
-                <div class="row">
-                    
+            <div class="tab-pane {{(app('request')->input('tab') == '') || (app('request')->input('tab') == 'overview') ? 'active': ''}}" role="tabpanel" id="tab-1">
+
+                <h3 class="mt-4">{{$event->name}} Overview</h3>
+
+                <hr>
+            
+                <div class="row">                    
+                   
+                   <div class="col-md-4">
+                       <div class="card border-primary">
+                       
+                            <div class="card-header bg-primary text-white">
+                                <h5 class="mb-0">Event Details</h5>
+                            </div>
+    
+                            <div class="card-body">
+                                <table class="table">
+                                    <tr>
+                                        <td>Event Name:</td>
+                                        <td>{{$event->name}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Event Date:</td>
+                                        <td>{{$event->datetime}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Event Venue:</td>
+                                        <td>{{$event->venue_name}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Venue Address:</td>
+                                        <td>{{$event->venue_address}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Charity:</td>
+                                        <td>{{$event->charity}}</td>
+                                    </tr>
+                                </table>
+                            </div>
+
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary float-right">Edit Details</a>
+                            </div>
+                       
+                       </div>
+                   </div>
+
                 </div>
             </div>
-            <div class="tab-pane" role="tabpanel" id="tab-2">
+            <div class="tab-pane {{(app('request')->input('tab') == 'bouts') ? 'active': ''}}" role="tabpanel" id="tab-2">
                 <div class="row">
                    
                 </div>
             </div>
-            <div class="tab-pane" role="tabpanel" id="tab-3">
+            <div class="tab-pane {{(app('request')->input('tab') == 'applicants') ? 'active': ''}}" role="tabpanel" id="tab-3">
                 <div class="row">
-                   
+                   <h3>test</h3>
                 </div>
             </div>
-            <div class="tab-pane" role="tabpanel" id="tab-4">
+            <div class="tab-pane {{(app('request')->input('tab') == 'auction') ? 'active': ''}}" role="tabpanel" id="tab-4">
                 <div class="row">
                    
                 </div>
@@ -52,5 +96,31 @@
         </div>
     </div>
 </div>
+
+<!-- Edit Event Details Modal -->
+<div class="modal fade" id="addToGroupModal" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Add a new contact to {{$group->name}}</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <form method="post" action="{{route('admin.group.addMember', [$group->id])}}">
+                    <div class="form-group">
+                        <label for="inviteeName">Name</label>
+                        <input type="text" name="name" id="inviteName" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="inviteeName">Email</label>
+                        <input type="email" name="email" id="inviteEmail" class="form-control" required>
+                    </div>
+                    @csrf
+                    <button type="submit" class="btn btn-success"><i class="fas fa-user-plus"></i> Add person to Group</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div> <!-- End Edit Event Details Modal -->
 
 @endsection
