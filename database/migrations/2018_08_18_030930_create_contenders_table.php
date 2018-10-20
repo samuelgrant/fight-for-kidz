31fd15721b6e06c332b7fe1084a5da492b80a052
jacksos101 <jacksos101@gmail.com>
2018-10-21 11:26:03 +1300

    Removed fields from contender that already exist in applicant table

--- database/migrations/2018_08_18_030930_create_contenders_table.php
+++ database/migrations/2018_08_18_030930_create_contenders_table.php
@@ -15,21 +15,18 @@ class CreateContendersTable extends Migration
     {
         Schema::create('contenders', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('event_id')->unsigned();
+            // $table->integer('event_id')->unsigned();
             $table->integer('sponsor_id')->unsigned()->nullable();
             $table->integer('applicant_id')->unsigned();
             $table->string('team'); // should be 'red' or 'blue'
-            $table->string('first_name');
-            $table->string('last_name');
             $table->string('nickname')->nullable();
-            $table->date('dob');
             $table->float('weight')->nullable();
             $table->float('height')->nullable();
             $table->float('reach')->nullable();
             $table->timestamps();
 
             // Foreign key constraints definition
-            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+            // $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
             $table->foreign('sponsor_id')->references('id')->on('sponsors')->onDelete('set null');
             $table->foreign('applicant_id')->references('id')->on('applicants')->onDelete('cascade');            
         });
