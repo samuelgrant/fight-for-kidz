4dd5cad4e517187cc2f5bf32aca0f272c91685bd
jacksos101 <jacksos101@gmail.com>
2018-10-21 11:25:37 +1300

    Added addToTeam function for applicant model

--- app/Applicant.php
+++ app/Applicant.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Contender;
 use App\Traits\Groupable;
 use Carbon\Carbon;
 
@@ -22,12 +23,53 @@ class Applicant extends Model
         return $this->hasOne('App\Contender');
     }
 
-    // Return true if a contender record exists for this application
-    // aka was this applicant accepted.
+    /**
+     * Returns true if the applicant has an associated
+     * contender record and the contender is assigned to 
+     * a team.
+     */
     
     public function isContender(){
-        return $this->contender != null;
+
+        if($this->contender != null){
+            return $this->contender->team != null;
+        }
+
+        return false;
     }
+
+    /**
+     *  Creates a contender record for this applicant.
+     */
+    public function addToTeam($team){
+
+        // check whether a contender already exists
+        if($this->contender == null){
+
+            $contender = new Contender;
+            $contender->applicant_id = $this->id;
+            $contender->weight = $this->expected_weight;
+            $contender->height = $this->height;
+            $contender->nickname = $this->preferred_nickname;
+            $contender->team = $team;
+            $contender->save();
+
+            // set foreign key field on contender
+            $this->contender()->associate($contender);
+
+        }
+        else { //contender record already exists, change team only
+
+            $contender = $this->contender;
+            $contender->team = $team;
+            $contender->save();
+        }
+
+    }
+
+    /**
+     *  Returns the age of the applicant
+     */
 
     public function getAge(){
         $date = Carbon::parse($this->dob);
