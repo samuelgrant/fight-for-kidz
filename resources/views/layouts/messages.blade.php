166b6c576429da7b498ae9de0f7a5760d6c89fbb
jacksos101 <jacksos101@gmail.com>
2018-10-23 16:01:43 +1300

    Public pages now dispaly session flash messages

--- resources/views/layouts/messages.blade.php
+++ resources/views/layouts/messages.blade.php
@@ -9,3 +9,12 @@
         {{session('error')}}
     </div>
 @endif
+
+@if(count($errors) > 0 )
+   @foreach($errors->all() as $error)
+     <div role="alert" class="alert alert-danger">
+       <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&#10007;</span></button>
+        {{$error}}
+     </div>
+   @endforeach
+@endif
