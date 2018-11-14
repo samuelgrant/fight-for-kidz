1125784053539b74f158b30283fda65ca2c45c31
jacksos101 <jacksos101@gmail.com>
2018-11-15 10:59:56 +1300

    Form index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,8 +17,8 @@
   	</header>
   	<!-- Upcoming event -->
 	<div style="background-color: black;">
-    	<section id="about" class="upcoming-section">
-			<div class="container">
+    	<section id="about" class="upcoming-section pt-5">
+			<div class="container pt-5">
 				<div class="row mb-5 pb-5 d-flex">
 					<div class="col-lg-12 col-ms-12 col-sm-12">
 						<h1 class="text-white underline bar">About</h1>
@@ -42,7 +42,7 @@
 		<!-- Subscriber Section -->
 		<section class="text-center" id="subscriber-section">
 	  
-		  	<div class="container my-5">
+		  	<div class="container conform p-3 my-5">
 				<h1 class="mb-3">Fight for Kidz Updates!</h1>
 				@include('layouts.messages')
 				<form method="post" action="{{route('subscribe')}}" class="justify-content-center">
