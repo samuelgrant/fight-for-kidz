646bb8ac1201d4fa43c4b1e0928ab275633c31f0
jacksos101 <jacksos101@gmail.com>
2018-11-20 21:02:02 +1300

    Rearranged group mgmt view a wee bit
    Needed extra width on the dtable for phone column

--- resources/views/admin/groupManagement.blade.php
+++ resources/views/admin/groupManagement.blade.php
@@ -14,78 +14,84 @@
 <!-- Page Content -->
 <div class="row">
     <!-- Group Settings -->
-    <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="card border-primary">
+    <div class="col-lg-12">
+        <div class="card border-primary mb-3">
             <div class="card-header bg-primary text-white">
                 <h5 class="mb-0"><i class="fas fa-exclamation-triangle"></i> Group Settings</h5>
             </div>
             <div class="card-body">
-                @if($group->type != "System Group" && !$group->deleted_at)
-                <form method="POST" action="{{route('admin.group.update', [$group->id])}}" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="name">Group Name</label>
-                        <input type="text" name="groupName" id="name" class="form-control" value="{{$group->name}}"
-                            required>
+                <div class="row">
+                    <div class="col-md-6">
+                        @if($group->type != "System Group" && !$group->deleted_at)
+                        <form method="POST" action="{{route('admin.group.update', [$group->id])}}" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label for="name">Group Name</label>
+                                <input type="text" name="groupName" id="name" class="form-control" value="{{$group->name}}"
+                                    required>
+                            </div>
+    
+                            <div class="form-group">
+                                <label for="groupAvatar">Optional Group Icon</label>
+                                <div class="row px-3">
+                                    <div class="mr-3 mb-3">
+                                        <img id="imgPreview" class="group-icon float-left mr-2" src="{{$group->custom_icon ? '/storage/images/groups/'.$group->id.'.png' : 'https://via.placeholder.com/80x100'}}"
+                                            alt="placeholder">
+                                    </div>
+                                    <div class="float-right">
+                                        <label class="btn btn-info btn-sm btn-file">
+                                            <i class="fas fa-upload"></i> Select Image
+                                            <input name="groupImage" id="groupImage" type="file" style="display: none;">
+                                            <input type="checkbox" id="removeImageCheckbox" name="removeImageCheckbox" style="display:none">
+                                        </label>
+                                        <button class="btn btn-danger btn-sm d-block" type="button" id="btnRemoveImage" onclick="resetImagePre()"><i
+                                                class="fas fa-times"></i>
+                                            Remove Image</button>
+                                        <small id="groupAvatarHelp" class="text-muted d-bhlock">Use png 100H x 80W.</small>
+                                    </div>
+                                </div>
+                            </div>
+    
+                            @csrf
+                            {{Form::hidden('_method', 'PUT')}}
                     </div>
-
-                    <div class="form-group">
-                        <label for="groupAvatar">Optional Group Icon</label>
-                        <div class="row px-3">
-                            <div class="mr-3 mb-3">
-                                <img id="imgPreview" class="group-icon" src="{{$group->custom_icon ? '/storage/images/groups/'.$group->id.'.png' : 'https://via.placeholder.com/80x100'}}"
-                                    class="float-left mr-2" alt="placeholder">
-                            </div>
-                            <div class="float-right">
-                                <label class="btn btn-info btn-sm btn-file">
-                                    <i class="fas fa-upload"></i> Select Image
-                                    <input name="groupImage" id="groupImage" type="file" style="display: none;">
-                                    <input type="checkbox" id="removeImageCheckbox" name="removeImageCheckbox" style="display:none">
-                                </label>
-                                <button class="btn btn-danger btn-sm d-block" type="button" id="btnRemoveImage" onclick="resetImagePre()"><i
-                                        class="fas fa-times"></i>
-                                    Remove Image</button>
-                                <small id="groupAvatarHelp" class="text-muted d-bhlock">Use png 100H x 80W.</small>
-                            </div>
+                        <div class="col-md-6">
+                            <button type="submit" class="btn btn-success btn-block"><i class="far fa-check-circle"></i> Update
+                                Settings</button>
+                        </form>
+    
+                        <hr>
+                        @if($group->type != "System Group" && !$group->deleted_at)
+                        {!!Form::open(['action'=>['admin\GroupManagementController@destroy', $group->id], 'method'=> 'POST'])
+                        !!}
+                        <button class="btn btn-danger btn-block" type="submit"><i class="fas fa-user-times"></i> Delete Group</button>
+                        {{Form::hidden('_method', 'delete')}}
+                        {!! Form::close() !!}
+                        <p class="mt-2">You Will not be able to:</p>
+                        <ul class="pl-2">
+                            <li>Add or remove people from the group.</li>
+                            <li>Email this group.</li>
+                            <li>Manage this group.</li>
+                        </ul>
+                        <p>You can re-enable this group from the deleted tab on the group management page.</p>
+                        @endif
+                        @else
+                        @if($group->deleted_at)
+                        <span class="text-danger">This group is disabled, you cannot change it's details unless it is restored first. </span>
+                        {!!Form::open(['action'=>['admin\GroupManagementController@restore', $group->id], 'method'=> 'POST'])
+                        !!}
+                        <button class="btn btn-info btn-block mt-3" type="submit"><i class="far fa-check-circle"></i> Restore Group</button>
+                        {{Form::hidden('_method', 'patch')}}
+                        {!! Form::close() !!}
+                        @else
+                        <span class="text-danger">This is an automated group. You cannot change the settings of this group.</span>
+                        @endif
+                        @endif
                         </div>
-                    </div>
-
-                    @csrf
-                    {{Form::hidden('_method', 'PUT')}}
-                    <button type="submit" class="btn btn-success btn-block"><i class="far fa-check-circle"></i> Update
-                        Settings</button>
-                </form>
-
-                <hr>
-                @if($group->type != "System Group" && !$group->deleted_at)
-                {!!Form::open(['action'=>['admin\GroupManagementController@destroy', $group->id], 'method'=> 'POST'])
-                !!}
-                <button class="btn btn-danger btn-block" type="submit"><i class="fas fa-user-times"></i> Delete Group</button>
-                {{Form::hidden('_method', 'delete')}}
-                {!! Form::close() !!}
-                <p class="mt-2">You Will not be able to:</p>
-                <ul class="pl-2">
-                    <li>Add or remove people from the group.</li>
-                    <li>Email this group.</li>
-                    <li>Manage this group.</li>
-                </ul>
-                <p>You can re-enable this group from the deleted tab on the group management page.</p>
-                @endif
-                @else
-                @if($group->deleted_at)
-                <span class="text-danger">This group is disabled, you cannot change it's details unless it is restored first. </span>
-                {!!Form::open(['action'=>['admin\GroupManagementController@restore', $group->id], 'method'=> 'POST'])
-                !!}
-                <button class="btn btn-info btn-block mt-3" type="submit"><i class="far fa-check-circle"></i> Restore Group</button>
-                {{Form::hidden('_method', 'patch')}}
-                {!! Form::close() !!}
-                @else
-                <span class="text-danger">This is an automated group. You cannot change the settings of this group.</span>
-                @endif
-                @endif
+                </div>
             </div>
         </div>
     </div>
-    <div class="col-lg-8 col-md-6 col-sm-12">
+    <div class="col-lg-12">
 
         {{-- Add selected to group - visible to all groups --}}
         <button class="btn btn-primary mb-3" type="button" data-toggle="modal" data-target="#copyToGroupModal">
@@ -104,7 +110,8 @@
                 <tr>
                     <th><input type="checkbox" id="dtable-select-all"></th>
                     <th class="dtable-control">Name</th>
-                    <th class="dtable-control">Email</th>
+                    <th class="dtable-control">Phone</th>
+                    <th class="dtable-control">Email</th>                    
                 </tr>
             </thead>
             <tbody>
@@ -117,6 +124,7 @@
                         </div>
                     </td>
                     <td>{{$member['name']}}</td>
+                    <td>{{$member['phone']}}</td>
                     <td><a href="mailto:{{$member['email']}}">{{$member['email']}}</a></td>
                 </tr>
                 @endforeach
@@ -140,6 +148,10 @@
                         <label for="inviteeName">Name</label>
                         <input type="text" name="name" id="inviteName" class="form-control" required>
                     </div>
+                    <div class="form-group">
+                            <label for="phone">Phone</label>
+                            <input type="text" name="phone" id="phone" class="form-control">
+                        </div>
                     <div class="form-group">
                         <label for="inviteeName">Email</label>
                         <input type="email" name="email" id="inviteEmail" class="form-control" required>
