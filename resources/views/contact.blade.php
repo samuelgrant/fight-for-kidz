489c550393db4d7dc434e6504a97d5f00bbf68ac
jacksos101 <jacksos101@gmail.com>
2018-11-22 23:41:08 +1300

    Fixed sticky footer
    Replaced dogy js script with CSS fix

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container conform">
-    <div class="push-down" style="padding: 20px 40px;">
+  <div class="container conform mt-5">
+    <div style="padding: 20px 40px;">
       <h1 class="text-white text-center">Contact Us</h1>
       <p class="text-white text-center mb-5">Please feel free to contact us and we will get back to you as soon as possible.</p>
       <form>
