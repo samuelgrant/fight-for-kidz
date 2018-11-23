00478591ecd20fd3d15e86065139787b5e131bd6
MitchellQ <40969453+MitchellQ@users.noreply.github.com>
2018-11-23 13:35:29 +1300

    finished merch front end

--- resources/views/Merchandise.blade.php
+++ resources/views/Merchandise.blade.php
@@ -10,14 +10,13 @@
         @if($item->item_visible)
         <div class="col-lg-4 merch-div">
             <div class="card">
-                <div  class="mb-4 text-center"><img class="img-fluid" style="height: 250px;" src="" /></div>
                 <div class="item-details-fixed">
-                    <h3 class="mb-2">{{$item->name}}</h5>
+                    <h3 class="mb-5">{{$item->name}}</h5>
 					<img class="mb-2" src="{{file_exists(public_path('/storage/images/merchandise/' . $item->id . '.png')) ? '/storage/images/merchandise/' . $item->id . '.png' : '/storage/images/noImage.png'}}"
 					width="200" max-height="100">
 					<p><i>{{$item->tagline}}</i></p>
 					<p>{{$item->desc}}</p>					
-					<p class="align-bottom">$&nbsp;{{$item->price}}</p>
+					<h4><b>${{$item->price}}</b></h4>
 				</div>
             </div>
         </div>
