9530cdc3de010d796b6566ba8bafc2e1eb58773f
jacksos101 <jacksos101@gmail.com>
2018-11-27 12:08:22 +1300

    Create views (website and email) for subscribe functions

---
+++ app/Mail/Unsubscribed.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use App\Subscriber;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Unsubscribed extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $subscriber;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Subscriber $subscriber)
+    {
+        $this->subscriber = $subscriber;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.subscribers.unsubscribed');
+    }
+}
---
+++ resources/views/feedback/unsubscribed.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container auth conform p-3">
+    <div class="text-center text-white">
+        <i class="fas fa-sad-tear fa-5x"></i>
+        <h1 class="pt-2 mb-5">Unsubscribed</h1>
+        <span>You will no longer received email updates from Fight for Kidz. We are sorry to see you go.</span>
+        <a href="/"><h3 class="text-center text-white mt-5"><u>Back to Fight for Kidz</u></h3></a>
+    </div>    
+</div>
+@endsection
