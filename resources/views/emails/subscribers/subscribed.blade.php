c6a92be4040538f457c841ab772140eef49faad1
jacksos101 <jacksos101@gmail.com>
2018-11-27 15:24:32 +1300

    Set up views for subscriber emails / forms

---
+++ resources/views/emails/subscribers/subscribed.blade.php
@@ -0,0 +1,27 @@
+@extends('emails.template')
+@section('mailSection')
+    <tr>
+        <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px/29px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;">
+            Greetings {{$subscriber->name}},
+        </td>        
+    </tr>
+    <tr>
+        <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px/29px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;">
+            You have successfully subscribed to updates from Fight for Kidz.
+        </td>        
+    </tr>
+    <tr>
+        <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px/29px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;">
+            If this is a mistake, please click the 'unsubscribe' link at the bottom of this email.
+        </td>        
+    </tr>
+    <tr>
+        <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px/29px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;">
+            <br> Regards, <br> <br> Fight for Kidz
+        </td>        
+    </tr> 
+@endsection
+
+@section('unsubscribe')
+    <a href="{{env('APP_URL') . '/unsubscribe?token=' . $subscriber->unsubscribe_token}}">Unsubscribe</a>
+@endsection
