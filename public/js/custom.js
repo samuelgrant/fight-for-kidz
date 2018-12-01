317abc93d01e50d98ad7dd62a48c2c26182660da
MitchellQ <40969453+MitchellQ@users.noreply.github.com>
2018-12-01 19:51:28 +1300

    Update custom.js
    added app form wizard js

--- public/js/custom.js
+++ public/js/custom.js
@@ -214,3 +214,13 @@ function auctionItemModal(id){
       console.log(error);
   });
 }
+
+//Application form wizard
+$(document).ready(function() {
+  $('#rootwizard').bootstrapWizard({onTabShow: function(tab, navigation, index) {
+  var $total = navigation.find('li').length;
+  var $current = index+1;
+  var $percent = ($current/$total) * 100;
+  $('#rootwizard .progress-bar').css({width:$percent+'%'});
+  }});
+});
