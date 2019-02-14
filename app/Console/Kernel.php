54c24a300c28c0459034887b5d1d8cc75b03cae9
jacksos101 <jacksos101@gmail.com>
2019-02-14 14:50:57 +1300

    Removed unused code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,34 +29,6 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-
-        Log::debug('scheduler run at ' . now());
-
-        $schedule->command('queue:work --tries=3')->everyMinute()->withoutOverlapping();
-    }
-
-    /**
-     *  Checks if a proces with $needle in the name is running
-     *  https://gist.github.com/mauris/11375869
-     * (not currently being used)
-     */
-    protected function isProcessRunning($needle){
-
-        // get process status
-        exec('ps aux -ww', $process_status);
-
-        // search for $needle
-        $result = array_filter($process_status, function($var) use ($needle){
-            return strpos($var, $needle);
-        });
-
-        // if less than 3 workers (each instance will result in 2 array entries) 
-        if(count($result) < 6){
-            return false;
-        } else{
-            return true;
-        }
-
     }
 
     /**
