01f33953fe23811c3fbf4a2afba15468b41d013d
jacksos101 <jacksos101@gmail.com>
2019-02-17 00:41:29 +1300

    Updated email template for contact confirmation

--- resources/views/emails/contact/contactReceived.blade.php
+++ resources/views/emails/contact/contactReceived.blade.php
@@ -2,17 +2,23 @@
 @section('mailSection')
     <tr>
         <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px/29px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;">
-            Dear {{$name}},
+            Greetings {{$recipient}},
         </td>        
     </tr>
     <tr>
         <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px/29px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;">
-            Thank you for you enquiry, we have received it and will be in touch with you.
+            @if($enquiryType == 'general')
+                Thank you for getting in touch with us, we'll get right back to you as soon as we can.
+            @elseif($enquiryType == 'sponsor')
+                Thank you for your interest in sponsoring Fight for Kidz, we will get in touch with you soon.
+            @elseif($enquiryType == 'table')
+                Thank you for your interest in booking a table at Fight for Kidz. We'll get right back to you as soon as we can.
+            @endif
         </td>        
     </tr>
     <tr>
         <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px/29px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;">
-            Regards, <br> <br> Fight for Kidz
+            <br> Regards, <br> <br> Fight for Kidz
         </td>        
-    </tr>
+    </tr> 
 @endsection
--- resources/views/emails/contact/contactReceivedPlain.blade.php
+++ resources/views/emails/contact/contactReceivedPlain.blade.php
@@ -1,6 +1,12 @@
-Dear {{$name}},
+Greetings {{$recipient}},
 
-Thank you for you enquiry, we have received it and will be in touch with you.
+@if($enquiryType == 'general')
+    Thank you for getting in touch with us, we'll get right back to you as soon as we can.
+@elseif($enquiryType == 'sponsor')
+    Thank you for your interest in sponsoring Fight for Kidz, we will get in touch with you soon.
+@elseif($enquiryType == 'table')
+    Thank you for your interest in booking a table at Fight for Kidz. We'll get right back to you as soon as we can.
+@endif
 
 Regards,
 
