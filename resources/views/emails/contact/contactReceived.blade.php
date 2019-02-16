53e6ccd72d548b4aef2b7ad1b1dd7c8d1779a07f
jacksos101 <jacksos101@gmail.com>
2019-02-17 00:46:30 +1300

    Fix variable names

--- resources/views/emails/contact/contactReceived.blade.php
+++ resources/views/emails/contact/contactReceived.blade.php
@@ -2,16 +2,16 @@
 @section('mailSection')
     <tr>
         <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px/29px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;">
-            Greetings {{$recipient}},
+            Greetings {{$name}},
         </td>        
     </tr>
     <tr>
         <td data-color="text" data-size="size text" data-min="10" data-max="26" data-link-color="link text color" data-link-style="font-weight:bold; text-decoration:underline; color:#40aceb;" align="left" style="font:16px/29px Arial, Helvetica, sans-serif; color:#888; padding:0 0 21px;">
-            @if($enquiryType == 'general')
+            @if($type == 'general')
                 Thank you for getting in touch with us, we'll get right back to you as soon as we can.
-            @elseif($enquiryType == 'sponsor')
+            @elseif($type == 'sponsor')
                 Thank you for your interest in sponsoring Fight for Kidz, we will get in touch with you soon.
-            @elseif($enquiryType == 'table')
+            @elseif($type == 'table')
                 Thank you for your interest in booking a table at Fight for Kidz. We'll get right back to you as soon as we can.
             @endif
         </td>        
--- resources/views/emails/contact/contactReceivedPlain.blade.php
+++ resources/views/emails/contact/contactReceivedPlain.blade.php
@@ -1,10 +1,10 @@
-Greetings {{$recipient}},
+Greetings {{$name}},
 
-@if($enquiryType == 'general')
+@if($type == 'general')
     Thank you for getting in touch with us, we'll get right back to you as soon as we can.
-@elseif($enquiryType == 'sponsor')
+@elseif($type == 'sponsor')
     Thank you for your interest in sponsoring Fight for Kidz, we will get in touch with you soon.
-@elseif($enquiryType == 'table')
+@elseif($type == 'table')
     Thank you for your interest in booking a table at Fight for Kidz. We'll get right back to you as soon as we can.
 @endif
 
