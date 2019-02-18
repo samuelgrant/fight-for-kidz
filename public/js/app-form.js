39d18e2095ef19a6e6300fd79c0ea4fcbf35a6cc
MitchellQ <40969453+MitchellQ@users.noreply.github.com>
2019-02-19 08:52:34 +1300

    added function to change default enter action from submit form to focus next input

--- public/js/app-form.js
+++ public/js/app-form.js
@@ -563,3 +563,16 @@ function canSubmit(){
     $("#appSubmitBtn").addClass('d-none');
   }
 }
+
+$(document).ready(function(){
+  $('form input').keydown(function (e) {
+    if (e.keyCode == 13) {
+        var inputs = $(this).parents("form").eq(0).find(":input");
+        if (inputs[inputs.index(this) + 1] != null) {                    
+            inputs[inputs.index(this) + 1].focus();
+        }
+        e.preventDefault();
+        return false;
+    }
+});
+});
