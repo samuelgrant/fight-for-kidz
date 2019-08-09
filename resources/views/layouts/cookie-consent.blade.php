f526bf2e5be01ef4e303594d4502525386d5f7e9
Samuel <samueljegrant@outlook.com>
2019-08-09 17:23:54 +1200

    Responsive updates
    Improved responsiveness of the cookie consent banner.

--- resources/views/layouts/cookie-consent.blade.php
+++ resources/views/layouts/cookie-consent.blade.php
@@ -1,6 +1,6 @@
 @if(Cookie::get('cookieconsent') === null)
-<div id="cookieConsent" class="fixed-bottom alert alert-cookies alert-dismissible fade show" role="alert">
-    <img src="/storage/images/f4k_logo_noyear.png" class="float-left" alt="Footer Logo" />
+<div id="cookieConsent" class="fixed-bottom alert alert-cookies alert-dismissible fade show d-none d-md-block" role="alert">
+    <img src="/storage/images/f4k_logo_noyear.png" class="float-left d-none d-lg-block" alt="Footer Logo" />
     <button type="button" class="btn btn-primary bg-blue float-right" onclick="acceptCookies();">
             <span aria-hidden="true">Accept</span>
     </button>
