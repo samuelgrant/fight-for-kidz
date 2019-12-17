a7ea993b04cc01e2881a46c9c27740eefdac26d6
Samuel <samueljegrant@outlook.com>
2019-12-17 18:27:52 +1300

    Fixed sitemap priorities
    Fixed the priority values to comply with google requirements

--- resources/views/_sitemap.blade.php
+++ resources/views/_sitemap.blade.php
@@ -6,7 +6,7 @@
     <url>
         <loc>{{env('APP_URL')}}/fighter-application</loc>
         <lastmod>{{substr(App\Event::current()->updated_at, 0, strpos(App\Event::current()->updated_at, ' '))}}</lastmod>
-        <priority>9.9</priority>
+        <priority>1.0</priority>
     </url>
     @endif
 
@@ -15,7 +15,7 @@
     <url>
         <loc>{{env('APP_URL')}}/event/{{str_replace(' ', '-', App\Event::current()->name)}}</loc>
         <lastmod>{{substr(App\Event::current()->updated_at, 0, strpos(App\Event::current()->updated_at, ' '))}}</lastmod>
-        <priority>9.5</priority>
+        <priority>0.9</priority>
     </url>
     @endif
 
@@ -23,17 +23,17 @@
     <url>
         <loc>{{env('APP_URL')}}</loc>
         <lastmod>{{substr($index->updated_at, 0, strpos($index->updated_at, ' '))}}</lastmod>
-        <priority>9.0</priority>
+        <priority>0.9</priority>
     </url>    
 
     <!-- Contact Us -->
     <url>
         <loc>{{env('APP_URL')}}/contact/general</loc>
-        <priority>8.0</priority>
+        <priority>0.8</priority>
     </url>
     
     {{-- Get a start priority of 7 --}}
-    <?php $priority = 7.0; ?>
+    <?php $priority = 0.7; ?>
 
     @foreach($events as $event)
         @if(!$event->isFutureEvent() || !App\Event::current()->isFutureEvent() || $event->id != App\Event::current()->id)
@@ -41,10 +41,10 @@
         <url>
             <loc>{{env('APP_URL')}}/event/{{str_replace(' ', '-', $event->name)}}</loc>
             <lastmod>{{substr($event->updated_at, 0, strpos($event->updated_at, ' '))}}</lastmod>
-            <priority>{{($priority >= 1) ? $priority : 1}}</priority>
+            <priority>{{($priority >= 0.0) ? $priority : 0.1}}</priority>
         </url>
         {{-- This value should go down by 0.25 each cycle --}}
-        <?php $priority = $priority - 0.25; ?>
+        <?php $priority = $priority - 0.1; ?>
         @endif
     @endforeach
 </urlset>
