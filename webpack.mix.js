f5e009747d493a14de2b5a0b87154db886ac88f9
Samuel Grant <samueljegrant@outlook.com>
2021-01-25 21:00:34 +1300

    Fixed Font Awesome, Webpack & Dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,9 @@ let mix = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
- */
-
-mix.react('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+ */   
+mix.js('resources/assets/js/app.js', 'public/js')
+   .react()
+   .sass('resources/assets/sass/app.scss', 'public/css', [
+   //
+]);
